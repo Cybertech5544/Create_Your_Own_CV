288314416049ffd1c453f7a5f471b2d5df8de704
Ritesh Saha <151867934+Cybertech5544@users.noreply.github.com>
2024-07-07 20:32:19 +0530

    Create_Your_Own_CV directory

--- Create_Your_Own_CV/script.js
+++
@@ -1,6 +0,0 @@
-let profileepic = document.getElementById("profilepic")
-let inputfile = document.getElementById("dp")
-
-inputfile.onchange = function () {
-profileepic.src = URL.createObjectURL(inputfile.files[0]);
-}
